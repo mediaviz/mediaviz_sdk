10a31a1e810fbbbcac6872097b83391fbb129989
calebbalbera <caleb.balbera@gmail.com>
2026-06-10 13:24:11 -0400

    chore: remove obsolete basic_sdk/v1.0.2
    Superseded by sdk/ (public) and admin-sdk/ (private) split.

--- basic_sdk/v1.0.2/javascript/index.js
+++
@@ -1,7 +0,0 @@
-export { MediaViz } from './MediaViz.js';
-export * from './errors.js';
-export * from './_oauth.js';
-export * from './photos.js';
-export * from './photoupload.js';
-export * from './projects.js';
-export * from './users.js';
